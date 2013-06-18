be31d9a62ebcce5bdf028d4f35a6774a02f6abba
Dahua Lin <lindahua@gmail.com>
2013-06-18 14:48:24 -0500

    add MappedArray1

--- src/vmap.jl
+++ src/vmap.jl
@@ -1,14 +1,32 @@
 # map operations to elements
 
-# one argument
+# mapped array
 
-function vmap!(op::UnaryFunctor, dst::AbstractArray, x::AbstractArray)
-	for i in 1 : length(dst)
-		dst[i] = evaluate(op, x[i])
-	end
-	dst
+import Base.getindex, Base.size, Base.length
+
+immutable MappedArray1{Op<:UnaryFunctor, A1<:AbstractArray} <: AbstractArray
+	op::Op
+	a1::A1
 end
 
+length(m::MappedArray1) = length(m.a1)
+size(m::MappedArray1) = size(m.a1)
+size(m::MappedArray1, d::Integer) = size(m.a1, d)
+
+getindex(m::MappedArray1, i::Integer) = evaluate(m.op, getindex(m.a1, i))
+getindex(m::MappedArray1, i::Integer, j::Integer) = evaluate(m.op, getindex(m.a1, i, j))
+
+
+# one argument
+
+# function vmap!(op::UnaryFunctor, dst::AbstractArray, x::AbstractArray)
+# 	for i in 1 : length(dst)
+# 		dst[i] = evaluate(op, x[i])
+# 	end
+# 	dst
+# end
+
+vmap!(op::UnaryFunctor, dst::AbstractArray, x::AbstractArray) = copy!(dst, MappedArray1(op, x))
 vmap!(op::UnaryFunctor, x::AbstractArray) = vmap!(op, x, x)
 
 vmap{T}(op::UnaryFunctor, x::AbstractArray{T}) = vmap!(op, Array(result_type(op, T), size(x)), x)
