b8dbbd6813f2167806d4572137ff0c18c929bd25
Dahua Lin <lindahua@gmail.com>
2013-06-20 16:37:57 -0500

    partially updated benchmark_reduce.jl

--- test/benchmark_reduce.jl
+++ test/benchmark_reduce.jl
@@ -1,6 +1,30 @@
 # Benchmark on reduction
 
-const oldperf = Array(Vector{Float64})
+# macros
+
+macro bench_reduc1(ptname, name, rp, f, a)
+	quote
+		println("    on $($name) ...")
+
+		($f)($a)
+		t0 = @elapsed for i in 1 : ($rp)
+			($f)($a) 
+		end
+
+		($f)($a, 1)
+		t1 = @elapsed for i in 1 : ($rp)
+			($f)($a, 1) 
+		end
+
+		($f)($a, 2)
+		t2 = @elapsed for i in 1 : ($rp)
+			($f)($a, 2)
+		end
+
+		push!($ptname, ($name, [t0, t1, t2]))
+	end
+end
+
 
 # data preparation
 
@@ -17,9 +41,39 @@ b3 = rand(1000, 1000) + 0.5
 #
 #################################################
 
-sum(a2, 2)
-@time for i in 1 : 10 sum(a2, 2) end
+typealias DIMS Union(Int, (Int, Int))
 
+_max(x::Array) = max(x)
+_max(x::Array, d::DIMS) = max(x, (), d)
+
+_min(x::Array) = min(x)
+_min(x::Array, d::DIMS) = min(x, (), d)
+
+_asum(x::Array) = sum(abs(x))
+_asum(x::Array, d::DIMS) = sum(abs(x), d)
+
+_amax(x::Array) = max(abs(x))
+_amax(x::Array, d::DIMS) = max(abs(x), (), d)
+
+_amin(x::Array) = min(abs(x))
+_amin(x::Array, d::DIMS) = min(abs(x), (), d)
+
+_sqsum(x::Array) = sum(abs2(x))
+_sqsum(x::Array, d::DIMS) = sum(abs2(x), d)
+
+# benchmark
+
+const oldperf = Array((ASCIIString, Vector{Float64}), 0)
+
+println("Benchmark results on Base methods:")
+
+@bench_reduc1 oldperf "sum" 10 sum a2
+@bench_reduc1 oldperf "max" 10 _max a2
+@bench_reduc1 oldperf "min" 10 _min a2
+@bench_reduc1 oldperf "asum" 10 _asum a2
+@bench_reduc1 oldperf "amax" 10 _amax a2
+@bench_reduc1 oldperf "amin" 10 _amin a2
+@bench_reduc1 oldperf "sqsum" 10 _sqsum a2
 
 #################################################
 #
@@ -28,8 +82,13 @@ sum(a2, 2)
 #################################################
 
 using NumericFunctors
+const newperf = Array((ASCIIString, Vector{Float64}), 0)
 
-sum(a2, 2)
-@time for i in 1 : 10 sum(a2, 2) end
+println("Benchmark results in New methods:")
 
+@bench_reduc1 newperf "sum" 10 sum a2
+
+println(oldperf)
+
+println(newperf)
 
