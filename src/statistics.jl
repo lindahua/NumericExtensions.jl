40e7e0dec48a35ba5327f97f9e9ca72afa2f32bc
Dahua Lin <lindahua@gmail.com>
2013-07-21 20:49:21 -0500

    numerical stable var & std

--- src/statistics.jl
+++ src/statistics.jl
@@ -26,7 +26,7 @@ function mean{T<:Real}(x::ContiguousArray{T}, dims::DimSpec)
 	multiply!(r, c)
 end
 
-function mean!{R<:Real,T<:Real}(dst::ContiguousArray{R}, x::Array{T}, dims::DimSpec)
+function mean!{R<:Real,T<:Real}(dst::ContiguousArray{R}, x::ContiguousArray{T}, dims::DimSpec)
 	@check_nonempty("mean")
 	c = convert(R, inv(_reduc_dim_length(x, dims)))
 	multiply!(sum!(dst, x, dims), c)
@@ -152,31 +152,33 @@ end
 #
 ###################
 
-function _var{R<:FloatingPoint, T<:Real}(::Type{R}, x::Array{T}, ifirst::Int, ilast::Int)
-	xi = x[ifirst]
-	s = convert(R, xi)
-	s2 = convert(R, xi * xi)
-
-	for i in ifirst+1 : ilast
-		@inbounds xi = x[i]
-		s += xi
-		s2 += xi * xi
-	end
-
+function _var{R<:FloatingPoint, T<:Real}(::Type{R}, x::ContiguousArray{T}, ifirst::Int, ilast::Int)
+	s = zero(R)
+	s2 = zero(R)
+	
 	nm1 = ilast - ifirst
 	n = nm1 + 1
-	mu = s / n
-	max((s2 - n * (mu * mu)) / nm1, zero(R))
-end
+	rg = ifirst : ilast
 
+	for i in rg
+		@inbounds xi = x[i]
+		s += xi
+	end
+	mu = s / n
+
+	for i in rg
+		@inbounds xi = x[i]
+		s2 += abs2(xi - mu)
+	end
+	s2 / nm1
+end
 
 function var{T<:Real}(x::Array{T})
 	@check_nonempty("var")
 	_var(to_fptype(T), x, 1, length(x))
 end
 
-
-function var!{R<:FloatingPoint,T<:Real}(dst::Array{R}, x::Vector{T}, dim::Int)
+function var!{R<:FloatingPoint,T<:Real}(dst::ContiguousArray{R}, x::ContiguousVector{T}, dim::Int)
 	if dim == 1
 		dst[1] = var(x)
 	else
@@ -185,48 +187,7 @@ function var!{R<:FloatingPoint,T<:Real}(dst::Array{R}, x::Vector{T}, dim::Int)
 	dst
 end
 
-function _varimpl_firstdim!{R<:FloatingPoint,T<:Real}(dst::ContiguousArray{R}, x::ContiguousArray{T}, m::Int, n::Int)
-	o = 0
-	for j in 1 : n
-		dst[j] = _var(R, x, o+1, o+m)
-		o += m
-	end
-end
-
-function _varimpl_lastdim!{R<:FloatingPoint,T<:Real}(dst::ContiguousArray{R}, s::Array{R}, x::ContiguousArray{T}, m::Int, n::Int)
-	for i in 1 : m
-		@inbounds xi = x[i]
-		@inbounds s[i] = xi
-		@inbounds dst[i] = xi * xi
-	end
-
-	o = m
-	for j in 2 : n
-		for i in 1 : m
-			@inbounds xi = x[o + i]
-			@inbounds s[i] += xi
-			@inbounds dst[i] += xi * xi
-		end
-		o += m
-	end
-
-	inv_n = one(R) / convert(R, n)
-	inv_nm1 = one(R) / convert(R, n - 1)
-	for i in 1 : m
-		@inbounds mu = s[i] * inv_n
-		dst[i] = max(dst[i] - n * (mu * mu), zero(R)) * inv_nm1
-	end
-end
-
-function _varimpl_middim!{R<:FloatingPoint,T<:Real}(dst::ContiguousArray{R}, s::Array{R}, x::ContiguousArray{T}, m::Int, n::Int, k::Int)
-	_varimpl_lastdim!(dst, s, x, m, n)
-	for l in 2 : k
-		_varimpl_lastdim!(unsafe_view(dst, :, l), s, unsafe_view(x, :, :, l), m, n)
-	end
-end
-
-
-function var!{R<:FloatingPoint,T<:Real}(dst::Array{R}, x::Array{T}, dim::Int)
+function var!{R<:FloatingPoint,T<:Real}(dst::ContiguousArray{R}, x::ContiguousArray{T}, dim::Int)
 	@check_nonempty("var")
 	nd = ndims(x)
 	if !(1 <= dim <= nd)
@@ -235,13 +196,15 @@ function var!{R<:FloatingPoint,T<:Real}(dst::Array{R}, x::Array{T}, dim::Int)
 	siz = size(x)
 
 	if dim == 1
-		_varimpl_firstdim!(dst, x, siz[1], _trail_length(siz, dim))
-	elseif dim == nd
-		prelen = _precede_length(siz, dim)
-		_varimpl_lastdim!(dst, Array(R, prelen), x, prelen, siz[dim])
+		o = 0
+		m = siz[1]
+		n = _trail_length(siz, 1)
+		for j in 1 : n
+			dst[j] = _var(R, x, o+1, o+m)
+			o += m
+		end		
 	else
-		prelen = _precede_length(siz, dim)
-		_varimpl_middim!(dst, Array(R, prelen), x, prelen, siz[dim], _trail_length(siz, dim))
+		varm!(dst, x, mean(x, dim), dim)
 	end
 	dst
 end
