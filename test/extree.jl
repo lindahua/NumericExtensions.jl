f3ff95b15211ce1ec640c1bb0e09c39e886eb16e
Dahua Lin <lindahua@gmail.com>
2013-10-13 20:01:37 -0500

    add tests over is_scalar_expr

--- test/extree.jl
+++ test/extree.jl
@@ -8,6 +8,7 @@ import NumericExtensions.is_binary_ewise
 import NumericExtensions.is_binary_sewise
 import NumericExtensions.is_unary_reduc
 import NumericExtensions.is_binary_reduc
+import NumericExtensions.is_scalar_expr
 
 import NumericExtensions.AbstractExpr
 import NumericExtensions.EwiseExpr
@@ -73,55 +74,76 @@ import NumericExtensions.EColon
 
 #### Expression elements
 
+## EConst
+
 x = extree(4.5)
 @test isa(x, EConst{Float64})
 @test x.value == 4.5
+@test is_scalar_expr(x)
+
+## EVar
 
 x = extree(:a)
 @test isa(x, EVar)
-@test !x.isscalar
 @test x.sym == :(a)
+@test !is_scalar_expr(x)
+
+x = EVar(:b, true)
+@test x.sym == :(b)
+@test is_scalar_expr(x)
+
+## ERange
 
 x = extree(:(1:3))
 @test isa(x, ERange)
 @test x.args == (extree(1), extree(3))
+@test !is_scalar_expr(x)
 
 x = extree(:(u:))
 @test isa(x, ERange)
 @test x.args == (extree(:u), EEnd())
+@test !is_scalar_expr(x)
 
 x = extree(:(1:2:n))
 @test isa(x, ERange)
 @test x.args == (extree(1), extree(2), extree(:n))
+@test !is_scalar_expr(x)
 
 x = extree(:(a:2:))
 @test isa(x, ERange)
 @test x.args == (extree(:a), extree(2), EEnd())
+@test !is_scalar_expr(x)
 
 x = extree(:(a[1]))
 @test isa(x, ERef)
 @test x.arr == extree(:a)
 @test x.args == (extree(1),)
+@test !is_scalar_expr(x)
 
-x = extree(:(a[i]))
+x = extree(:(a[i, j]))
 @test isa(x, ERef)
 @test x.arr == extree(:a)
-@test x.args == (extree(:i),)
+@test x.args == (extree(:i), extree(:j))
+@test !is_scalar_expr(x)
 
 x = extree(:(a[i1:i2, j, 1]))
 @test isa(x, ERef)
 @test x.arr == extree(:a)
 @test x.args == (extree(:(i1:i2)), extree(:j), extree(1))
+@test !is_scalar_expr(x)
 
 x = extree(:(a[:]))
 @test isa(x, ERef)
 @test x.arr == extree(:a)
 @test x.args == (EColon(),)
+@test !is_scalar_expr(x)
 
 x = extree(:(a[:,i,:]))
 @test isa(x, ERef)
 @test x.arr == extree(:a)
 @test x.args == (EColon(), EVar(:i), EColon())
+@test !is_scalar_expr(x)
+
 
 
 
