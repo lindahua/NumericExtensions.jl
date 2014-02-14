be691023fbb39d4b87aed7e45f87acee655dd732
Dahua Lin <lindahua@gmail.com>
2014-02-14 15:55:16 -0600

    saccum kernel done

--- src/rkernels.jl
+++ src/rkernels.jl
@@ -27,34 +27,17 @@ function safe_min(x)
 end
 
 
-### sum
+### core skeleton
 
-suminit{T<:Number}(::Type{T}) = zero(T)
-suminit{T<:Unsigned}(::Type{T}) = uint(0)
-suminit{T<:Signed}(::Type{T}) = 0
-suminit(::Type{Int64}) = int64(0)
-suminit(::Type{Uint64}) = uint64(0)
-suminit(::Type{Int128}) = int128(0)
-suminit(::Type{Uint128}) = uint128(0)
-suminit(::Type{Bool}) = 0
- 
-suminit{T<:Number}(::Type{T}) = zero(T)
-suminit{T<:Unsigned}(::Type{T}) = uint(0)
-suminit{T<:Signed}(::Type{T}) = 0
-suminit(::Type{Int64}) = int64(0)
-suminit(::Type{Uint64}) = uint64(0)
-suminit(::Type{Int128}) = int128(0)
-suminit(::Type{Uint128}) = uint128(0)
-suminit(::Type{Bool}) = 0
- 
-function vecsum{T}(n::Int, a::Array{T}, ia::Int)
-    v::T
+abstract Reduc
+
+function saccum{Op<:Reduc}(::Type{Op}, n::Int, a::Array, ia::Int)
     ia_end = ia + n
     @inbounds if n <= 4
         v = a[ia]
         ia += 1
         while ia < ia_end
-            v += a[ia]
+            v = combine(Op, v, a[ia])
             ia += 1
         end
     else
@@ -65,29 +48,128 @@ function vecsum{T}(n::Int, a::Array{T}, ia::Int)
         ia += 4
         ia_ = ia_end - 3
         while ia < ia_
-            v1 += a[ia]
-            v2 += a[ia+1]
-            v3 += a[ia+2]
-            v4 += a[ia+3]
+            v1 = combine(Op, v1, a[ia])
+            v2 = combine(Op, v2, a[ia+1])
+            v3 = combine(Op, v3, a[ia+2])
+            v4 = combine(Op, v4, a[ia+3])
             ia += 4
         end
-        v = v1 + v2 + v3 + v4
+        v = combine(Op, combine(Op, v1, v2), combine(Op, v3, v4))
         while ia < ia_end
-            v += a[ia]
+            v = combine(Op, v, a[ia])
             ia += 1
         end 
     end
     return v
 end
  
-function vecsum{T}(n::Int, a::Array{T}, ia::Int, sa::Int)
-    v::T
+function saccum{Op<:Reduc}(::Type{Op}, n::Int, fun::Functor{1}, a::Array, ia::Int)
+    ia_end = ia + n
+    @inbounds if n <= 4
+        v = evaluate(fun, a[ia])
+        ia += 1
+        while ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia]))
+            ia += 1
+        end
+    else
+        v1 = evaluate(fun, a[ia])
+        v2 = evaluate(fun, a[ia+1])
+        v3 = evaluate(fun, a[ia+2])
+        v4 = evaluate(fun, a[ia+3])
+        ia += 4
+        ia_ = ia_end - 3
+        while ia < ia_
+            v1 = combine(Op, v1, evaluate(fun, a[ia]))
+            v2 = combine(Op, v2, evaluate(fun, a[ia+1]))
+            v3 = combine(Op, v3, evaluate(fun, a[ia+2]))
+            v4 = combine(Op, v4, evaluate(fun, a[ia+3]))
+            ia += 4
+        end
+        v = combine(Op, combine(Op, v1, v2), combine(Op, v3, v4))
+        while ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia]))
+            ia += 1
+        end 
+    end
+    return v
+end
+
+function saccum{Op<:Reduc}(::Type{Op}, n::Int, fun::Functor{2}, a::Array, ia::Int, b::Array, ib::Int)
+    ia_end = ia + n
+    @inbounds if n <= 4
+        v = evaluate(fun, a[ia], b[ib])
+        ia += 1
+        ib += 1
+        while ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia], b[ib]))
+            ia += 1
+            ib += 1
+        end
+    else
+        v1 = evaluate(fun, a[ia], b[ib])
+        v2 = evaluate(fun, a[ia+1], b[ib+1])
+        ia += 2
+        ib += 2
+        ia_ = ia_end - 1
+        while ia < ia_
+            v1 = combine(Op, v1, evaluate(fun, a[ia], b[ib]))
+            v2 = combine(Op, v2, evaluate(fun, a[ia+1], b[ib+1]))
+            ia += 2
+            ib += 2
+        end
+        v = combine(Op, v1, v2)
+        if ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia], b[ib]))
+        end 
+    end
+    return v
+end
+
+function saccum{Op<:Reduc}(::Type{Op}, n::Int, fun::Functor{3}, a::Array, ia::Int, b::Array, ib::Int, c::Array, ic::Int)
+    ia_end = ia + n
+    @inbounds if n <= 4
+        v = evaluate(fun, a[ia], b[ib], c[ic])
+        ia += 1
+        ib += 1
+        ic += 1
+        while ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia], b[ib], c[ic]))
+            ia += 1
+            ib += 1
+            ic += 1
+        end
+    else
+        v1 = evaluate(fun, a[ia], b[ib], c[ic])
+        v2 = evaluate(fun, a[ia+1], b[ib+1], c[ic+1])
+        ia += 2
+        ib += 2
+        ic += 2
+        ia_ = ia_end - 1
+        while ia < ia_
+            v1 = combine(Op, v1, evaluate(fun, a[ia], b[ib], c[ic]))
+            v2 = combine(Op, v2, evaluate(fun, a[ia+1], b[ib+1], c[ic+1]))
+            ia += 2
+            ib += 2
+            ic += 2
+        end
+        v = combine(Op, v1, v2)
+        if ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia], b[ib], c[ic]))
+        end 
+    end
+    return v
+end
+
+
+
+function saccum{Op<:Reduc}(::Type{Op}, n::Int, a::Array, ia::Int, sa::Int)
     ia_end = ia + sa * n
     @inbounds if n <= 4
         v = a[ia]
         ia += sa
         while ia < ia_end
-            v += a[ia]
+            v = combine(Op, v, a[ia])
             ia += sa
         end
     else
@@ -97,52 +179,177 @@ function vecsum{T}(n::Int, a::Array{T}, ia::Int, sa::Int)
         ia += sa2
         ia_ = ia_end - sa
         while ia < ia_
-            v1 += a[ia]
-            v2 += a[ia + sa]
+            v1 = combine(Op, v1, a[ia])
+            v2 = combine(Op, v2, a[ia + sa])
             ia += sa2
         end
-        v = v1 + v2
+        v = combine(Op, v1, v2)
         if ia < ia_end
-            v += a[ia]
-        end
+            v = combine(Op, v, a[ia])
+        end 
     end
     return v
 end
 
-function vecadd!(n::Int, d::Array, id::Int, a::Array, ia::Int)
+function saccum{Op<:Reduc}(::Type{Op}, n::Int, fun::Functor{1}, a::Array, ia::Int, sa::Int)
+    ia_end = ia + sa * n
+    @inbounds if n <= 4
+        v = evaluate(fun, a[ia])
+        ia += sa
+        while ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia]))
+            ia += sa
+        end
+    else
+        sa2 = 2sa
+        v1 = evaluate(fun, a[ia])
+        v2 = evaluate(fun, a[ia + sa])
+        ia += sa2
+        ia_ = ia_end - sa
+        while ia < ia_
+            v1 = combine(Op, v1, evaluate(fun, a[ia]))
+            v2 = combine(Op, v2, evaluate(fun, a[ia + sa]))
+            ia += sa2
+        end
+        v = combine(Op, v1, v2)
+        if ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia]))
+        end 
+    end
+    return v
+end
+
+function saccum{Op<:Reduc}(::Type{Op}, n::Int, fun::Functor{2}, a::Array, ia::Int, sa::Int, b::Array, ib::Int, sb::Int)
+    ia_end = ia + sa * n
+    @inbounds if n <= 4
+        v = evaluate(fun, a[ia], b[ib])
+        ia += sa
+        ib += sb
+        while ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia], b[ib]))
+            ia += sa
+            ib += sb
+        end
+    else
+        sa2 = 2sa
+        sb2 = 2sb
+        v1 = evaluate(fun, a[ia], b[ib])
+        v2 = evaluate(fun, a[ia + sa], b[ib + sb])
+        ia += sa2
+        ib += sb2
+        ia_ = ia_end - sa
+        while ia < ia_
+            v1 = combine(Op, v1, evaluate(fun, a[ia], b[ib]))
+            v2 = combine(Op, v2, evaluate(fun, a[ia + sa], b[ib + sb]))
+            ia += sa2
+            ib += sb2
+        end
+        v = combine(Op, v1, v2)
+        if ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia], b[ib]))
+        end 
+    end
+    return v
+end
+
+function saccum{Op<:Reduc}(::Type{Op}, n::Int, fun::Functor{3}, a::Array, ia::Int, sa::Int, 
+                                                                b::Array, ib::Int, sb::Int, 
+                                                                c::Array, ic::Int, sc::Int)
+    ia_end = ia + sa * n
+    @inbounds if n <= 4
+        v = evaluate(fun, a[ia], b[ib], c[ic])
+        ia += sa
+        ib += sb
+        ic += sc
+        while ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia], b[ib], c[ic]))
+            ia += sa
+            ib += sb
+            ic += sc
+        end
+    else
+        sa2 = 2sa
+        sb2 = 2sb
+        sc2 = 2sc
+        v1 = evaluate(fun, a[ia], b[ib], c[ic])
+        v2 = evaluate(fun, a[ia + sa], b[ib + sb], c[ic + sc])
+        ia += sa2
+        ib += sb2
+        ic += sc2
+        ia_ = ia_end - sa
+        while ia < ia_
+            v1 = combine(Op, v1, evaluate(fun, a[ia], b[ib], c[ic]))
+            v2 = combine(Op, v2, evaluate(fun, a[ia + sa], b[ib + sb], c[ic + sc]))
+            ia += sa2
+            ib += sb2
+            ic += sc2
+        end
+        v = combine(Op, v1, v2)
+        if ia < ia_end
+            v = combine(Op, v, evaluate(fun, a[ia], b[ib], c[ic]))
+        end 
+    end
+    return v
+end
+
+
+
+function paccum!{Op<:Reduc}(::Type{Op}, n::Int, d::Array, id::Int, a::Array, ia::Int)
     ia_end = ia + n
     ia_ = ia_end - 3
     @inbounds while ia < ia_
-        d[id] += a[ia]
-        d[id+1] += a[ia+1]
-        d[id+2] += a[ia+2]
-        d[id+3] += a[ia+3]
+        d[id] = combine(Op, d[id], a[ia])
+        d[id+1] = combine(Op, d[id+1], a[ia+1])
+        d[id+2] = combine(Op, d[id+2], a[ia+2])
+        d[id+3] = combine(Op, d[id+3], a[ia+3])
         ia += 4
         id += 4
     end
  
     @inbounds while ia < ia_end
-        d[id] += a[ia]
+        d[id] = combine(Op, d[id], a[ia])
         id += 1
         ia += 1
     end
 end
 
-function vecadd!(n::Int, d::Array, id::Int, sd::Int, a::Array, ia::Int, sa::Int)
+function paccum!{Op<:Reduc}(::Type{Op}, n::Int, d::Array, id::Int, sd::Int, a::Array, ia::Int, sa::Int)
     ia_end = ia + sa * n
-    ia_ = ia_end - sa
     sa2 = 2sa
     sd2 = 2sd
-
+    ia_ = ia_end - sa
     @inbounds while ia < ia_
-        d[id] += a[ia]
-        d[id + sd] += a[ia + sa]
+        d[id] = combine(Op, d[id], a[ia])
+        d[id + sd] = combine(Op, d[id+sd], a[ia + sa])
         ia += sa2
         id += sd2
     end
  
     @inbounds if ia < ia_end
-        d[id] += a[ia]
+        d[id] = combine(Op, d[id], a[ia])
     end
 end
+
+
+### reduction operations
+
+type Sum <: Reduc end
+
+init{T<:Number}(::Type{Sum}, ::Type{T}) = zero(T)
+init{T<:Unsigned}(::Type{Sum}, ::Type{T}) = uint(0)
+init{T<:Signed}(::Type{Sum}, ::Type{T}) = 0
+init(::Type{Sum}, ::Type{Int64}) = int64(0)
+init(::Type{Sum}, ::Type{Uint64}) = uint64(0)
+init(::Type{Sum}, ::Type{Int128}) = int128(0)
+init(::Type{Sum}, ::Type{Uint128}) = uint128(0)
+init(::Type{Sum}, ::Type{Bool}) = 0
+
+combine(::Type{Sum}, x, y) = x + y
+
+
+
+
+
+
+
 
--- test/rkernels.jl
+++ test/rkernels.jl
@@ -1,32 +1,50 @@
 # testing of reduction kernels
 
+using NumericExtensions
 import NumericExtensions: safe_sum, safe_max, safe_min
-import NumericExtensions: vecsum, vecadd!
+import NumericExtensions: saccum, paccum!
+import NumericExtensions: Sum
 using Base.Test
 
-a = rand(60)
-b = rand(60)
+a = randn(100)
+b = randn(100)
+c = randn(100)
 
 for n = 1:15
-    @test_approx_eq vecsum(n, a, 1) safe_sum(a[1:n])
-    @test_approx_eq vecsum(n, a, 3) safe_sum(a[3:n+2])
-    @test_approx_eq vecsum(n, a, 1, 2) safe_sum(a[1:2:2n-1])
-    @test_approx_eq vecsum(n, a, 3, 2) safe_sum(a[3:2:2n+1])
+    @test_approx_eq saccum(Sum, n, a, 1) safe_sum(a[1:n])
+    @test_approx_eq saccum(Sum, n, a, 3) safe_sum(a[3:n+2])
+    @test_approx_eq saccum(Sum, n, a, 1, 2) safe_sum(a[1:2:2n-1])
+    @test_approx_eq saccum(Sum, n, a, 3, 2) safe_sum(a[3:2:2n+1])
+
+    @test_approx_eq saccum(Sum, n, Abs2Fun(), a, 1) safe_sum(abs2(a[1:n]))
+    @test_approx_eq saccum(Sum, n, Abs2Fun(), a, 3) safe_sum(abs2(a[3:n+2]))
+    @test_approx_eq saccum(Sum, n, Abs2Fun(), a, 1, 2) safe_sum(abs2(a[1:2:2n-1]))
+    @test_approx_eq saccum(Sum, n, Abs2Fun(), a, 3, 2) safe_sum(abs2(a[3:2:2n+1]))
+
+    @test_approx_eq saccum(Sum, n, Multiply(), a, 1, b, 1) safe_sum(.*(a[1:n], b[1:n]))
+    @test_approx_eq saccum(Sum, n, Multiply(), a, 3, b, 4) safe_sum(.*(a[3:n+2], b[4:n+3]))
+    @test_approx_eq saccum(Sum, n, Multiply(), a, 1, 2, b, 1, 3) safe_sum(.*(a[1:2:2n-1], b[1:3:3n-2]))
+    @test_approx_eq saccum(Sum, n, Multiply(), a, 3, 2, b, 4, 3) safe_sum(.*(a[3:2:2n+1], b[4:3:3n+1]))
+
+    @test_approx_eq saccum(Sum, n, FMA(), a, 1, b, 1, c, 1) safe_sum(fma(a[1:n], b[1:n], c[1:n]))
+    @test_approx_eq saccum(Sum, n, FMA(), a, 3, b, 4, c, 5) safe_sum(fma(a[3:n+2], b[4:n+3], c[5:n+4]))
+    @test_approx_eq saccum(Sum, n, FMA(), a, 1, 2, b, 1, 3, c, 1, 4) safe_sum(fma(a[1:2:2n-1], b[1:3:3n-2], c[1:4:4n-3]))
+    @test_approx_eq saccum(Sum, n, FMA(), a, 3, 2, b, 4, 3, c, 5, 4) safe_sum(fma(a[3:2:2n+1], b[4:3:3n+1], c[5:4:4n+1]))
 end
 
 for n = 1:15
     r = copy(b[1:n])
-    vecadd!(n, r, 1, a, 1)
+    paccum!(Sum, n, r, 1, a, 1)
     @test_approx_eq r b[1:n] + a[1:n]
 
     r = copy(b[1:n+3])
-    vecadd!(n, r, 4, a, 3)
+    paccum!(Sum, n, r, 4, a, 3)
     @test r[1:3] == b[1:3]
     @test_approx_eq r[4:n+3] b[4:n+3] + a[3:n+2]  
 
     r = copy(b[1:4+3n])
     rc = copy(r)
-    vecadd!(n, r, 4, 3, a, 3, 2)
+    paccum!(Sum, n, r, 4, 3, a, 3, 2)
     u = falses(4+3n)
     u[4:3:3n+1] = true
     @test r[~u] == rc[~u]
