98ddafb0d7c84e0528a6f0347da790256700518d
Dahua Lin <lindahua@gmail.com>
2014-02-16 09:15:39 -0600

    revised map.jl

--- src/map.jl
+++ src/map.jl
@@ -9,56 +9,47 @@
 # the macro to generate functor-based mapping functions 
 # for various number of arguments
 
-function code_mapfuns(nargs::Int)
-    asyms = [symbol("a$(d)") for d = 1 : nargs]
-    params = [Expr(:(::), a, :DenseArrOrNum) for a in asyms]
-    sargs = [:(getvalue($a, i)) for a in asyms]
-    sargs1 = [:(getvalue($a, 1)) for a in asyms]
+macro compose_mapfuns(fname, AN)
+    @assert AN != 0
+    FN = AN > 0 ? AN : 1
+    fname! = symbol(string(fname, '!'))
+
+    # code-gen preparation
+    h = codegen_helper(AN)
+    aargs = h.args[2:end]
+    cparams = h.contiguous_aparams[2:end]
+    aparams = h.dense_aparams[2:end]
+    ti = h.term(:i)
 
     quote
-        function map!(f::Functor{$nargs}, dst::NumericArray, $(params...))
+        global $(fname!)
+        function $(fname!)(fun::Functor{$FN}, dst::ContiguousArray, $(cparams...))
             n = length(dst)
-            n == maplength($(asyms...)) || error("Inconsistent argument dimensions.")
-            for i = 1 : n
-                @inbounds dst[i] = evaluate(f, $(sargs...))
+            n == $(h.inputlen) || throw(DimensionMismatch("Inconsistent argument lengths."))
+            for i = 1:n
+                @inbounds dst[i] = $(ti)
             end
             dst
         end
 
-        map1!(f::Functor{$nargs}, a1::NumericArray, $(params[2:end]...)) = map!(f, a1, $(asyms...))
-
-        function map(f::Functor{$nargs}, $(params...))
-            shp = mapshape($(asyms...))
-            n::Int = prod(shp)
-            reshape([(@inbounds y = evaluate(f, $(sargs...)); y) for i = 1 : n], shp)
-        end
+        global $(fname)
+        $(fname)(fun::Functor{$FN}, $(cparams...)) = 
+            $(fname!)(fun, Array($(h.termtype), $(h.inputsize)), $(aargs...))
     end
 end
- 
-macro mapfuns(nargs)
-    esc(code_mapfuns(nargs))
-end
 
-@mapfuns 1
-@mapfuns 2
-@mapfuns 3
-@mapfuns 4
-@mapfuns 5
+@compose_mapfuns map 1
+@compose_mapfuns map 2
+@compose_mapfuns map 3
+@compose_mapfuns mapdiff (-2)
 
-function mapdiff!(f::Functor{1}, dst::NumericArray, a1::DenseArrOrNum, a2::DenseArrOrNum)
-    n = length(dst)
-    n == maplength(a1, a2) || error("Inconsistent argument dimensions.")
-    for i = 1 : n
-        @inbounds dst[i] = evaluate(f, getvalue(a1, i) - getvalue(a2, i))
-    end
-    return dst
-end
-
-function mapdiff(f::Functor{1}, a1::DenseArrOrNum, a2::DenseArrOrNum)
-    shp = mapshape(a1, a2)
-    n::Int = prod(shp)
-    reshape([(@inbounds y = getvalue(a1, i) - getvalue(a2, i); evaluate(f, y)) for i = 1 : n], shp)
-end 
+map1!(f::Functor{1}, a::DenseArray) = map!(f, a, a)
+map1!(f::Functor{2}, a::DenseArray, b::DenseArray) = map!(f, a, a, b)
+map1!(f::Functor{2}, a::DenseArray, b::Number) = map!(f, a, a, b)
+map1!(f::Functor{3}, a::DenseArray, b::DenseArray, c::DenseArray) = map!(f, a, a, b, c)
+map1!(f::Functor{3}, a::DenseArray, b::DenseArray, c::Number) = map!(f, a, a, b, c)
+map1!(f::Functor{3}, a::DenseArray, b::Number, c::DenseArray) = map!(f, a, a, b, c)
+map1!(f::Functor{3}, a::DenseArray, b::Number, c::Number) = map!(f, a, a, b, c)
 
 
 #################################################
@@ -67,26 +58,46 @@ end
 #
 #################################################
 
-add!(x::NumericArray, y::DenseArrOrNum) = map1!(Add(), x, y)
-subtract!(x::NumericArray, y::DenseArrOrNum) = map1!(Subtract(), x, y)
-multiply!(x::NumericArray, y::DenseArrOrNum) = map1!(Multiply(), x, y)
-divide!(x::NumericArray, y::DenseArrOrNum) = map1!(Divide(), x, y)
+macro derive_mapfuns1(fname, Op, BT)
+    fname! = symbol(string(fname, '!'))
+    quote
+        global $(fname!)
+        $(fname!){T<:$(BT)}(a::DenseArray{T}) = map1!($(Op)(), a)
+        $(fname!){T<:$(BT)}(dst::DenseArray, a::DenseArray{T}) = map!($(Op)(), dst, a)
+    end
+end
 
-negate!(x::NumericArray) = map1!(Negate(), x)
-abs!(x::NumericArray) = map1!(AbsFun(), x)
-abs2!(x::NumericArray) = map1!(Abs2Fun(), x)
-sqr!(x::NumericArray) = map1!(Abs2Fun(), x)
-rcp!(x::NumericArray) = map1!(RcpFun(), x)
-sqrt!(x::NumericArray) = map1!(SqrtFun(), x)
-pow!(x::NumericArray, p::DenseArrOrNum) = map1!(Pow(), x, p)
+macro derive_mapfuns2(fname, Op, BT)
+    fname! = symbol(string(fname, '!'))
+    quote
+        global $(fname!)
+        $(fname!){T1<:$(BT)}(a::DenseArray{T1}, b::$(BT)) = map1!($(Op)(), a, b)
+        $(fname!){T1<:$(BT),T2<:$(BT)}(a::DenseArray{T1}, b::DenseArray{T2}) = map1!($(Op)(), a, b)
+        $(fname!){T1<:$(BT)}(dst::DenseArray, a::DenseArray{T1}, b::$(BT)) = map!($(Op)(), dst, a, b)
+        $(fname!){T1<:$(BT),T2<:$(BT)}(dst::DenseArray, a::DenseArray{T1}, b::DenseArray{T2}) = map!($(Op)(), dst, a, b)
+    end
+end
 
-floor!(x::NumericArray) = map1!(FloorFun(), x)
-ceil!(x::NumericArray) = map1!(CeilFun(), x)
-round!(x::NumericArray) = map1!(RoundFun(), x)
-trunc!(x::NumericArray) = map1!(TruncFun(), x)
+@derive_mapfuns1 negate Negate Number
 
-exp!(x::NumericArray) = map1!(ExpFun(), x)
-log!(x::NumericArray) = map1!(LogFun(), x)
+@derive_mapfuns2 add Add Number
+@derive_mapfuns2 subtract Subtract Number
+@derive_mapfuns2 multiply Multiply Number
+@derive_mapfuns2 divide Divide Number
+
+@derive_mapfuns1 abs AbsFun Number
+@derive_mapfuns1 abs2 Abs2Fun Number
+@derive_mapfuns1 rcp RcpFun Number
+@derive_mapfuns1 sqrt SqrtFun Real
+@derive_mapfuns2 pow Pow Real
+
+@derive_mapfuns1 floor FloorFun Real
+@derive_mapfuns1 ceil CeilFun Real
+@derive_mapfuns1 round RoundFun Real
+@derive_mapfuns1 trunc TruncFun Real
+
+@derive_mapfuns1 exp ExpFun Real
+@derive_mapfuns1 log LogFun Real
 
 # extensions
 
